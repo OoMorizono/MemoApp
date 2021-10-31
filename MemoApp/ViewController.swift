b55807df64467b457d06d57ece10f043feeddb4c
OoMorizono <x65f588d72sw76@gmail.com>
2021-10-31 11:56:22 +0900

    テスト

--- MemoApp/ViewController.swift
+++ MemoApp/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController,UITextViewDelegate {
     
     @IBOutlet weak var textView: UITextView!
 
-    
+    //テスト用のコメントだよ
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
