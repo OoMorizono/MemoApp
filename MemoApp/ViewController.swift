ac1e7472a4ea7fd5cc40b47e2c0b992569cb91c1
OoMorizono <x65f588d72sw76@gmail.com>
2021-10-31 11:40:49 +0900

    機能の追加

--- MemoApp/ViewController.swift
+++ MemoApp/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextViewDelegate {
+    
+    @IBOutlet weak var textView: UITextView!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textView.delegate = self
+   
+        // SceneDelegateが持っている前回の内容を呼び出してTextViewに表示されるようにします。
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
+        textView.text = sceneDelegate.lastText
     }
-
+    
+    func textViewDidChange(_ tetView: UITextView)
+    {
+        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
+        sceneDelegate.lastText = textView.text
+    }
+ 
 
 }
 
